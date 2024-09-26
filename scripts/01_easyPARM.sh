1080176b12c30845ffd189fb34fabedb062872c8
Abdelazim M. A. Abdelgawwad <abdelazim.abdelgawwad@uv.es>
2024-09-26 17:32:11 +0200

    scripts

--- scripts/01_easyPARM.sh
+++ scripts/01_easyPARM.sh
@@ -19,6 +19,7 @@
 #Copyright 2024 Abdelazim M. A. Abdelgawwad, Universitat de Valéncia. E-mail: abdelazim.abdelgawwad@uv.es         #
 ###################################################################################################################
 
+
 # Detect the directory where the current script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
