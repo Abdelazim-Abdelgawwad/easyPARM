73fe7f4316add987a4b81290a1f76e20a5d47107
Abdelazim M. A. Abdelgawwad <abdelazim.abdelgawwad@uv.es>
2024-09-26 16:59:31 +0200

    Update easyPARM.sh

--- easyPARM.sh
+++ easyPARM.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-
+###################################################################################################################
+#   Automated Force Fields for Metals     /$$$$$$$   /$$$$$$  /$$$$$$$  /$$      /$$                              # 
+#                                        | $$__  $$ /$$__  $$| $$__  $$| $$$    /$$$                              #
+#   /$$$$$$   /$$$$$$   /$$$$$$$ /$$   /$$| $$  \ $$| $$  \ $$| $$  \ $$| $$$$  /$$$$                             #
+#  /$$__  $$ |____  $$ /$$_____/| $$  | $$| $$$$$$$/| $$$$$$$$| $$$$$$$/| $$ $$/$$ $$                             #
+# | $$$$$$$$  /$$$$$$$|  $$$$$$ | $$  | $$| $$____/ | $$__  $$| $$__  $$| $$  $$$| $$                             #
+# | $$_____/ /$$__  $$ \____  $$| $$  | $$| $$      | $$  | $$| $$  \ $$| $$\  $ | $$                             #
+# |  $$$$$$$|  $$$$$$$ /$$$$$$$/|  $$$$$$$| $$      | $$  | $$| $$  | $$| $$ \/  | $$                             #
+#  \_______/ \_______/|_______/  \____  $$|__/      |__/  |__/|__/  |__/|__/     |__/                             #
+#                               /$$  | $$                                                                         #
+#                              |  $$$$$$/              Ver. 1.00 - 26 September 2024                              #
+#                               \______/                                                                          #
+#                                                                                                                 #
+# Developer: Abdelazim M. A. Abdelgawwad.                                                                         #
+# Institut de Ciència Molecular (ICMol), Universitat de València, P.O. Box 22085, ValÃ¨ncia 46071, Spain          #
+#                                                                                                                 #
+#Distributed under the GNU LESSER GENERAL PUBLIC LICENSE Version 2.1, February 1999                               #
+#Copyright 2024 Abdelazim M. A. Abdelgawwad, Universitat de València. E-mail: abdelazim.abdelgawwad@uv.es        #
+###################################################################################################################
 # Capture the directory from which this script is called
 ORIGINAL_DIR="$(pwd)"
 
